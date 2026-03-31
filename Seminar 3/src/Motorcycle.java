f92d5e38e2a35af4a56b48b548eed301b7c81305
Ghinea Gabriela <ghineagabriela24@stud.ase.ro>
2026-03-31 12:00:00 +0300

    Seminar 3

---
+++ Seminar 3/src/Motorcycle.java
@@ -0,0 +1,42 @@
+public class Motorcycle extends Vehicle {
+    private boolean hasSidecar;
+
+    public Motorcycle(String id, String brand, double speed, int mileage, boolean hasSidecar) {
+        super(id, brand, speed, mileage);
+        this.hasSidecar = hasSidecar;
+    }
+
+    public boolean hasSidecar() {
+        return hasSidecar;
+    }
+
+    public void setHasSidecar(boolean hasSidecar) {
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void move() {
+        System.out.println("The motorcycle is riding fast.");
+    }
+
+    @Override
+    public boolean needsService() {
+        return getMileage() >= 6000;
+    }
+
+    @Override
+    public double rentalPrice(int days) {
+        double price = 30 * days;
+
+        if (hasSidecar) {
+            price += 15 * days;
+        }
+
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Motorcycle -> " + super.toString() + ", Has sidecar: " + hasSidecar;
+    }
+}
